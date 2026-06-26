f6511ad8f675b852e835f9b6638ba6829bc7fc14
Yogaraj Senrayan <nirvanasolutions@Yogarajs-MacBook-Air.local>
2026-06-26 13:29:58 +0530

    Configure Supabase cloud connection

--- cloud-config.js
+++ cloud-config.js
@@ -1,4 +1,4 @@
 window.CLOUD_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://exnqqnsprxzuonoztqja.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4bnFxbnNwcnh6dW9ub3p0cWphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NTAzNzUsImV4cCI6MjA5ODAyNjM3NX0.j3slVw-08fcdoLzQoFFckJiqOL907qlt4q5u36U2wRk"
 };
